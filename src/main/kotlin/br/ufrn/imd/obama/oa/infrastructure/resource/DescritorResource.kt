69f99052ad35dbf2721256f014d20f5089867c91
Vitor Bezerra <vitorhenrique908@gmail.com>
2024-02-27 16:48:01 -0300

    OB-0009: Adiciona interface para mapeamento do resource no swagger

---
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/DescritorResource.kt
@@ -0,0 +1,28 @@
+package br.ufrn.imd.obama.oa.infrastructure.resource
+
+import br.ufrn.imd.obama.oa.infrastructure.resource.exchange.ListarNivelEnsinoResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+
+interface DescritorResource {
+
+    @Operation(summary = "Endpoint para listar descritores existente")
+    @ApiResponses(value = [
+        ApiResponse(
+            responseCode = "200",
+            description = "Descritores encontrados",
+            content = [
+                Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = Set::class)
+                )
+            ]
+        )
+    ])
+    fun listarNivelEnsino(): ResponseEntity<Set<ListarNivelEnsinoResponse>>
+}
