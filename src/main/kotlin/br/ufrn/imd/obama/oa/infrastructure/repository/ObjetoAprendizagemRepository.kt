212641f8138accc3f1034467fd4b143f1aa08add
Emanuel Kywal <emanuelkywal@outlook.com>
2024-04-10 16:43:20 -0400

    Adiciona procurarPorId paginada em repository

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/ObjetoAprendizagemRepository.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/ObjetoAprendizagemRepository.kt
@@ -11,6 +11,12 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface ObjetoAprendizagemRepository: JpaRepository<ObjetoAprendizagemEntity, Long> {
+
+    fun findById(
+        id: Long,
+        pageable: Pageable
+    ): Page<ObjetoAprendizagemEntity>
+
     @Query(
             " SELECT distinct oa FROM ObjetoAprendizagemEntity oa " +
                     " LEFT JOIN oa.descritores d " +
