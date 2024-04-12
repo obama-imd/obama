5e138a5f371e8d3eb3516af07aa13727d9dd9bc2
RamonJales <ramonjales123@gmail.com>
2024-04-11 21:37:59 -0300

    HabilidadeRepository criado

---
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/HabilidadeRepository.kt
@@ -0,0 +1,18 @@
+package br.ufrn.imd.obama.oa.infrastructure.repository
+
+import br.ufrn.imd.obama.oa.infrastructure.entity.HabilidadeEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface HabilidadeRepository: JpaRepository<HabilidadeEntity, Long> {
+
+    @Query("SELECT h FROM HabilidadeEntity h WHERE h.anoEnsino.id = :anoEnsinoId AND h.temaConteudo.id = :temaConteudoId")
+    fun buscarHabilidadesPorAnoDeEnsinoIdETemaConteudoId(@Param("anoEnsinoId") anoEnsinoId: Long
+                                                         , @Param("temaConteudoId") temaConteudoId: Long
+                                                         , pageable: Pageable): Page<HabilidadeEntity>
+}
