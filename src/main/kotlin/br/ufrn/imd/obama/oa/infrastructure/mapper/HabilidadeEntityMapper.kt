8a176f04da6695f721f9c5639f4bae4e607f95b3
RamonJales <ramonjales123@gmail.com>
2024-04-11 22:10:35 -0300

    método toModelV2 criado

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/mapper/HabilidadeEntityMapper.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/mapper/HabilidadeEntityMapper.kt
@@ -1,6 +1,7 @@
 package br.ufrn.imd.obama.oa.infrastructure.mapper
 
 import br.ufrn.imd.obama.oa.domain.model.Habilidade
+import br.ufrn.imd.obama.oa.domain.model.HabilidadeV2
 import br.ufrn.imd.obama.oa.infrastructure.entity.HabilidadeEntity
 
 fun HabilidadeEntity.toModel(): Habilidade {
@@ -11,5 +12,14 @@ fun HabilidadeEntity.toModel(): Habilidade {
             codigo = this.codigo,
             temaConteudo = this.temaConteudo.toModel(),
             anoEnsino = this.anoEnsino.toModel()
+    )
+}
+
+fun HabilidadeEntity.toModelV2(): HabilidadeV2 {
+    return HabilidadeV2(
+        id = this.id,
+        codigo = this.codigo,
+        descricao = this.descricao,
+        nomeAnoEnsino = this.anoEnsino.nome,
     )
 }
