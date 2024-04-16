88b68e736f465f5bc2ce4cb9c0c546cddf973fc0
RamonJales <ramonjales123@gmail.com>
2024-04-16 15:25:01 -0300

    HabilidadeResourceImpl test

---
+++ src/test/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/HabilidadeResourceImplTest.kt
@@ -0,0 +1,89 @@
+package br.ufrn.imd.obama.oa.infrastructure.resource
+
+import br.ufrn.imd.obama.oa.domain.model.Habilidade
+import br.ufrn.imd.obama.oa.domain.usecase.HabilidadeUseCase
+import br.ufrn.imd.obama.oa.infrastructure.adapter.HabilidadeGatewayAdapter
+import br.ufrn.imd.obama.oa.infrastructure.configuration.HabilidadeConfig
+import br.ufrn.imd.obama.oa.infrastructure.repository.HabilidadeRepository
+import br.ufrn.imd.obama.oa.util.criarHabilidade
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@ActiveProfiles(profiles = ["test"])
+@SpringBootTest(
+    classes = [
+        HabilidadeConfig::class,
+        HabilidadeGatewayAdapter::class,
+        HabilidadeUseCase::class,
+        HabilidadeRepository::class,
+        HabilidadeResourceImpl::class
+    ]
+)
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = [
+    DataSourceAutoConfiguration::class,
+    HibernateJpaAutoConfiguration::class,
+    DataSourceTransactionManagerAutoConfiguration::class
+])
+class HabilidadeResourceImplTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private  lateinit var habilidadeUseCase: HabilidadeUseCase
+
+    @MockBean
+    private lateinit var habilidadeGatewayAdapter: HabilidadeGatewayAdapter
+
+    @MockBean
+    private lateinit var habilidadeRepository: HabilidadeRepository
+
+    @Test
+    fun `Deve retornar ok quando lista descritores passando os dois parametros corretos`() {
+        var pageable: Pageable = Pageable.ofSize(10)
+
+        var resultado: Page<Habilidade> = PageImpl(
+            listOf(
+                criarHabilidade(),
+            ),
+        )
+
+        Mockito.`when`(
+            habilidadeUseCase.buscarHabilidadesPorAnoDeEnsinoIdETemaConteudoId(
+                1L,
+                1L,
+                pageable
+            )
+        ).thenReturn(
+            resultado
+        )
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/v1/habilidade")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("anoEnsinoId", "1L")
+                .param("temaConteudoId", "1L")
+                .param("page", "0")
+                .param("size", "10")
+        ).andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+    }
+}
