58da224bfe208e82eb96f9238db87a3ce7bd0344
RamonJales <ramonjales123@gmail.com>
2024-04-26 22:44:26 -0300

    usarioEntity update

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/entity/UsuarioEntity.java
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/entity/UsuarioEntity.java
@@ -1,6 +1,12 @@
 package br.ufrn.imd.obama.oa.infrastructure.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
 @Table(name = "usuario")
@@ -12,21 +18,26 @@ class UsuarioEntity(
 
         @column(name = "nome")
         val nome:String,
+
         @column(name = "sobrenome")
         val sobrenome:String,
+
         @column(name = "email")
         val email:String,
+
         @column(name = "senha")
         val senha:String,
+
         @column(name = "role")
         val role:Role,
+
         @column(name = "ativo")
         val ativo:Boolean,
+
         @column(name = "tipo_cadastro")
         val tipoCadastro:TipoCadastro,
+
         @column(name = "token")
         val token:String
 
-) {
-
-}
+)
