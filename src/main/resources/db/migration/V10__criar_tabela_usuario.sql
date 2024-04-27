0495e4558bf11088d6a9eabae3bd0b1a2feef148
RamonJales <ramonjales123@gmail.com>
2024-04-26 22:45:09 -0300

    script criar_tabela_usuario criado

---
+++ src/main/resources/db/migration/V10__criar_tabela_usuario.sql
@@ -0,0 +1,13 @@
+//cria a tabela usuario com id e nome obrigatorios
+CREATE TABLE Usuario (
+    id int8 NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    papel ENUM('ADMIN', 'PADRAO', 'REVISOR') NOT NULL,
+    ativo BOOLEAN NOT NULL,
+    tipoCadastro ENUM('PADRAO', 'GOOGLE', 'FACEBOOK') NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    CONSTRAINT usuario_pk PRIMARY KEY (id)
+);
