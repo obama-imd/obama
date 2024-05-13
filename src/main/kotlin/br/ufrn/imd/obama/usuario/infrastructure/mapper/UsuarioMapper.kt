f1e0ed23ef82d8018c566f85a69eef2862752ea5
RamonJales <ramonjales123@gmail.com>
2024-05-12 22:48:18 -0300

    toresponse do usuario criado

--- src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/mapper/UsuarioMapper.kt
+++ src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/mapper/UsuarioMapper.kt
@@ -1,7 +1,11 @@
 package br.ufrn.imd.obama.usuario.infrastructure.mapper
 
+import br.ufrn.imd.obama.usuario.domain.enums.Role
+import br.ufrn.imd.obama.usuario.domain.enums.TipoCadastro
 import br.ufrn.imd.obama.usuario.domain.model.Usuario
 import br.ufrn.imd.obama.usuario.infrastructure.entity.UsuarioEntity
+import br.ufrn.imd.obama.usuario.infrastructure.resource.exchange.UsuarioResponse
+import com.fasterxml.jackson.annotation.JsonProperty
 
 fun Usuario.toEnitty(): UsuarioEntity {
     return UsuarioEntity(
@@ -10,6 +14,19 @@ fun Usuario.toEnitty(): UsuarioEntity {
         sobrenome = this.sobrenome,
         email = this.email,
         senha = this.senha,
+        role = this.role,
+        ativo = this.ativo,
+        tipoCadastro = this.tipoCadastro,
+        token = this.token
+    )
+}
+
+fun Usuario.toResponse(): UsuarioResponse {
+    return UsuarioResponse(
+        id = this.id,
+        nome = this.nome,
+        sobrenome = this.sobrenome,
+        email = this.email,
         role = this.role,
         ativo = this.ativo,
         tipoCadastro = this.tipoCadastro,
