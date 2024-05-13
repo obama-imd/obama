34a30bf9640f9144895514d42463e39f7a39ad1d
RamonJales <ramonjales123@gmail.com>
2024-05-13 19:26:50 -0300

    UsuarioRepository

--- src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/repository/UsuarioRepository.kt
+++ src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/repository/UsuarioRepository.kt
@@ -2,7 +2,10 @@ package br.ufrn.imd.obama.usuario.infrastructure.repository
 
 import br.ufrn.imd.obama.usuario.infrastructure.entity.UsuarioEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UsuarioRepository: JpaRepository<UsuarioEntity, Long> {}
+interface UsuarioRepository: JpaRepository<UsuarioEntity, Long> {
+    fun findByLogin(login: String): UserDetails
+}
