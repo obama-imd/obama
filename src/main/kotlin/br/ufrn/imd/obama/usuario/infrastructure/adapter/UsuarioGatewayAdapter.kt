83c08e0032f2abe31b36559191b1be43d1563889
RamonJales <ramonjales123@gmail.com>
2024-05-13 19:26:34 -0300

    service sobrescrevendo método loadByUserName do UserDetailsService

--- src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/adapter/UsuarioGatewayAdapter.kt
+++ src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/adapter/UsuarioGatewayAdapter.kt
@@ -6,14 +6,20 @@ import br.ufrn.imd.obama.usuario.domain.model.Usuario
 import br.ufrn.imd.obama.usuario.infrastructure.entity.UsuarioEntity
 import br.ufrn.imd.obama.usuario.infrastructure.mapper.toModel
 import br.ufrn.imd.obama.usuario.infrastructure.repository.UsuarioRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
 class UsuarioGatewayAdapter (
     private val usuarioRepository: UsuarioRepository
-): UsuarioGateway {
+): UsuarioGateway, UserDetailsService {
     override fun salvarUsuario(usuario: Usuario): Usuario {
         val usuarioEntity: UsuarioEntity = usuario.toEnitty()
         return usuarioRepository.save(usuarioEntity).toModel()
+    }
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        return usuarioRepository.findByLogin(username ?: throw IllegalArgumentException("Usuario não pode ser nulo"))
     }
 }
