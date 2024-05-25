d70eca6d212556dbbd8288b6a2d07d44f6471897
Vitor Bezerra <vitorhenrique908@gmail.com>
2024-05-25 10:40:15 -0300

    OB-0023: Adiciona modelo de Plano de Aula

---
+++ src/main/kotlin/br/ufrn/imd/obama/planoaula/domain/model/PlanoAula.kt
@@ -0,0 +1,32 @@
+package br.ufrn.imd.obama.planoaula.domain.model
+
+import br.ufrn.imd.obama.oa.domain.model.AnoEnsino
+import br.ufrn.imd.obama.oa.domain.model.Disciplina
+import br.ufrn.imd.obama.oa.domain.model.NivelEnsino
+import br.ufrn.imd.obama.oa.domain.model.ObjetoAprendizagem
+import br.ufrn.imd.obama.planoaula.domain.enums.StatusPlanoAula
+import br.ufrn.imd.obama.usuario.domain.model.Usuario
+import java.time.LocalDateTime
+
+data class PlanoAula(
+    val id: Long,
+    val dataCadastro: LocalDateTime,
+    private var qtdDownload: Int = 0,
+    private val escola: String?,
+    private var duracaoEmMinutos: Int? = null,
+    private var titulo: String? = null,
+    private var resumo: String? = null,
+    private var objetivoGeral: String? = null,
+    private var objetivosEspecificos: String? = null,
+    private var metodologia: String? = null,
+    private var avaliacao: String? = null,
+    private var referencias: String? = null,
+    private var token: String? = null,
+    private var status: StatusPlanoAula,
+    private var autor: Usuario? = null,
+    private var nivelEnsino: NivelEnsino? = null,
+    private var disciplinasEnvolvidas: List<Disciplina>? = null,
+    private var anoEnsino: AnoEnsino? = null,
+    private var objetosAprendizagem: Set<ObjetoAprendizagem>? = null,
+    private var coautores: Set<Usuario>? = null,
+)
