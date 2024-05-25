e458048f1a359ce1efcc94ed8eb159e3d29ed498
RamonJales <ramonjales123@gmail.com>
2024-05-25 13:08:19 -0300

    usuarioResponse criado

---
+++ src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/resource/exchange/UsuarioResponse.kt
@@ -0,0 +1,20 @@
+package br.ufrn.imd.obama.usuario.infrastructure.resource.exchange
+
+import br.ufrn.imd.obama.usuario.domain.enums.Papel
+import br.ufrn.imd.obama.usuario.domain.enums.TipoCadastro
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(value= PropertyNamingStrategies.SnakeCaseStrategy::class)
+class UsuarioResponse (
+    val id: Long,
+    val nome: String,
+    val sobrenome: String,
+    val email: String,
+    val role: Papel,
+    val ativo: Boolean,
+    @JsonProperty("tipo_cadastro")
+    val tipoCadastro: TipoCadastro,
+    val token: String,
+)
