cb7623bfe0625113e1cec123425cb8d8484f692e
RamonJales <ramonjales123@gmail.com>
2024-05-25 13:11:16 -0300

    método salvar criado no resource

--- src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/resource/AuthenticationResource.kt
+++ src/main/kotlin/br/ufrn/imd/obama/usuario/infrastructure/resource/AuthenticationResource.kt
@@ -1,7 +1,10 @@
 package br.ufrn.imd.obama.usuario.infrastructure.resource
 
+import br.ufrn.imd.obama.usuario.domain.model.Usuario
+import br.ufrn.imd.obama.usuario.infrastructure.handler.UsuarioExceptionHandler
 import br.ufrn.imd.obama.usuario.infrastructure.resource.exchange.LoginRequest
 import br.ufrn.imd.obama.usuario.infrastructure.resource.exchange.LoginResponse
+import br.ufrn.imd.obama.usuario.infrastructure.resource.exchange.UsuarioResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
@@ -28,4 +31,29 @@ interface AuthenticationResource {
     fun login(
         request: LoginRequest
     ): ResponseEntity<LoginResponse>
+
+    @Operation(summary = "Endpoint para salvar um usuário")
+    @ApiResponses(value = [
+        ApiResponse(
+            responseCode = "200",
+            description = "Usuário salvo com sucesso",
+            content = [
+                Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = UsuarioResponse::class)
+                )
+            ]
+        ),
+        ApiResponse(
+            responseCode = "400",
+            description = "Erro ao salvar usuário",
+            content = [
+                Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = UsuarioExceptionHandler::class)
+                )
+            ]
+        )
+    ])
+    fun salvarUsuario(usuario: Usuario): ResponseEntity<UsuarioResponse>
 }
