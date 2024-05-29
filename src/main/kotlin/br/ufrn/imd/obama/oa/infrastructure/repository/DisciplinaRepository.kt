b48141d74de4ac8fb776c3f4b7f042a5b46e8fa6
Emanuel Kywal <emanuelkywal@outlook.com>
2024-05-29 19:11:36 -0400

    Corrige tipo em DisciplinaRepository

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/DisciplinaRepository.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/DisciplinaRepository.kt
@@ -1,9 +1,10 @@
 package br.ufrn.imd.obama.oa.infrastructure.repository
 
+import br.ufrn.imd.obama.oa.infrastructure.entity.DisciplinaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface DisciplinaRepository : JpaRepository<DisciplinaRepository, Long> {
+interface DisciplinaRepository : JpaRepository<DisciplinaEntity, Long> {
 
 }
