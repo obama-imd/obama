f4355f921aaef5cdfac870ba22403ce7ae6312c6
Emanuel Kywal <emanuelkywal@outlook.com>
2024-06-04 17:16:52 -0300

    Adiciona mapper: PlanoAula -> PlanoAulaEntity

---
+++ src/main/kotlin/br/ufrn/imd/obama/planoaula/infrastructure/mapper/PlanoAulaMapper.kt
@@ -0,0 +1,31 @@
+package br.ufrn.imd.obama.planoaula.infrastructure.mapper
+
+import br.ufrn.imd.obama.oa.infrastructure.mapper.toEntity
+import br.ufrn.imd.obama.planoaula.domain.model.PlanoAula
+import br.ufrn.imd.obama.planoaula.infrastructure.entity.PlanoAulaEntity
+import br.ufrn.imd.obama.usuario.infrastructure.mapper.toEntity
+
+fun PlanoAula.toEntity(): PlanoAulaEntity {
+    return PlanoAulaEntity(
+        id = this.id,
+        dataCadastro = this.dataCadastro,
+        qtdDownload = this.qtdDownload,
+        escola = this.escola,
+        duracaoEmMinutos = this.duracaoEmMinutos,
+        titulo = this.titulo,
+        resumo = this.resumo,
+        objetivoGeral = this.objetivoGeral,
+        objetivosEspecificos = this.objetivosEspecificos,
+        metodologia = this.metodologia,
+        avaliacao = this.avaliacao,
+        referencias = this.referencias,
+        token = this.token,
+        status = this.status,
+        autor = this.autor?.toEntity(),
+        nivelEnsino = this.nivelEnsino?.toEntity(),
+        disciplinasEnvolvidas = this.disciplinasEnvolvidas?.map { it.toEntity() },
+        anoEnsino = this.anoEnsino?.toEntity(),
+        objetosAprendizagem = this.objetosAprendizagem?.map { it.toEntity() }?.toSet(),
+        coautores = this.coautores?.map { it.toEntity() }?.toSet()
+    )
+}
