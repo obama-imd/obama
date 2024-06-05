eae3d1850c350da662c5f293ee35e17a8e5a3de8
Emanuel Kywal <emanuelkywal@outlook.com>
2024-06-05 11:49:18 -0300

    Adiciona teste end-to-end da classe DisciplinaResourceImpl

---
+++ src/test/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/DisciplinaResourceImplTest.kt
@@ -0,0 +1,40 @@
+package br.ufrn.imd.obama.oa.infrastructure.resource
+
+import br.ufrn.imd.obama.oa.domain.usecase.DisciplinaUseCase
+import br.ufrn.imd.obama.oa.infrastructure.repository.DisciplinaRepository
+import jakarta.transaction.Transactional
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(profiles = ["test"])
+@Transactional
+class DisciplinaResourceImplTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var disciplinaUseCase: DisciplinaUseCase
+
+    @Test
+    fun `Deve retornar OK quando lista disciplinas`() {
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/v1/disciplina")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("page", "0")
+                .param("size", "10")
+        ).andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+    }
+}
