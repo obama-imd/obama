008ae1792f6dea61ccfffb630d4c7d3a15ae91aa
Emanuel Kywal <emanuelkywal@outlook.com>
2024-06-05 21:50:44 -0400

    Adiciona cache para endpoint listar disciplinas

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/DisciplinaResourceImpl.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/DisciplinaResourceImpl.kt
@@ -4,6 +4,7 @@ import br.ufrn.imd.obama.oa.domain.usecase.DisciplinaUseCase
 import br.ufrn.imd.obama.oa.infrastructure.mapper.toResponse
 import br.ufrn.imd.obama.oa.infrastructure.resource.exchange.ListarDisciplinaResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.MediaType
@@ -23,6 +24,7 @@ class DisciplinaResourceImpl(
     private val disciplinaUseCase: DisciplinaUseCase
 ) : DisciplinaResource {
 
+    @Cacheable(cacheNames = ["disciplinas"])
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     override fun listarDisciplinas(
         pageable: Pageable
