3fef513bfd297f9c949a25e622c9e5a1c52e4bce
Emanuel Kywal <emanuelkywal@outlook.com>
2024-06-06 14:48:36 -0300

    Adiciona annotation @repository em AnoEnsinoRepository

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/AnoEnsinoRepository.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/AnoEnsinoRepository.kt
@@ -2,6 +2,8 @@ package br.ufrn.imd.obama.oa.infrastructure.repository
 
 import br.ufrn.imd.obama.oa.infrastructure.entity.AnoEnsinoEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AnoEnsinoRepository : JpaRepository<AnoEnsinoEntity, Long> {
 }
