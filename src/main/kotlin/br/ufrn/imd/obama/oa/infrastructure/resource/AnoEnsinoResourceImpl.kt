ccc5291cb974759bab7210c2e2c72ca5e7cd639e
Emanuel Kywal <emanuelkywal@outlook.com>
2024-06-06 15:41:46 -0300

    Adiciona cache para endpoint de listagem de anos de ensino

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/AnoEnsinoResourceImpl.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/AnoEnsinoResourceImpl.kt
@@ -4,8 +4,10 @@ import br.ufrn.imd.obama.oa.domain.usecase.AnoEnsinoUseCase
 import br.ufrn.imd.obama.oa.infrastructure.mapper.toResponse
 import br.ufrn.imd.obama.oa.infrastructure.resource.exchange.ListarAnoEnsinoResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,7 +24,8 @@ class AnoEnsinoResourceImpl(
     private val anoEnsinoUseCase: AnoEnsinoUseCase
 ) : AnoEnsinoResource {
 
-    @GetMapping
+    @Cacheable(cacheNames = ["anosensino"])
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     override fun listarAnosEnsino(pageable: Pageable): Page<ListarAnoEnsinoResponse> {
         return anoEnsinoUseCase.listarAnosEnsino(pageable).map { it.toResponse() }
     }
