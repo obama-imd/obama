9d5c4988e3d87103c5f8d2ea4da1a8a8a9556295
RamonJales <ramonjales123@gmail.com>
2024-06-13 09:40:24 -0300

    usuarioResource teste

--- src/test/kotlin/br/ufrn/imd/obama/usuario/infrastructure/resource/UsuarioResourceImplTest.kt
+++ src/test/kotlin/br/ufrn/imd/obama/usuario/infrastructure/resource/UsuarioResourceImplTest.kt
@@ -4,16 +4,19 @@ import br.ufrn.imd.obama.usuario.domain.enums.Papel
 import br.ufrn.imd.obama.usuario.domain.enums.TipoCadastro
 import br.ufrn.imd.obama.usuario.domain.model.Usuario
 import br.ufrn.imd.obama.usuario.domain.usecase.UsuarioUseCase
+import br.ufrn.imd.obama.usuario.infrastructure.repository.UsuarioRepository
 import br.ufrn.imd.obama.usuario.infrastructure.resource.exchange.AtivarUsuarioRequest
 import br.ufrn.imd.obama.usuario.infrastructure.resource.exchange.CadastrarUsuarioRequest
 import br.ufrn.imd.obama.usuario.util.criarUsuarioInativo
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.transaction.Transactional
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.security.core.token.TokenService
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
@@ -33,7 +36,12 @@ class UsuarioResourceImplTest {
     @Autowired
     private lateinit var usuarioDatabaseUseCase: UsuarioUseCase
 
+    @Autowired
+    private lateinit var usarioRepository: UsuarioRepository
+
     private val objectMapper = ObjectMapper()
+
+    val tokenTeste = "teste"
 
     @Test
     fun `Deve retornar 201 created quando salvar um usuário com dados corretos`() {
@@ -95,44 +103,32 @@ class UsuarioResourceImplTest {
             .andExpect(status().isBadRequest)
     }
 
-//    @Test
-//    fun `deve retornar 204 No Content quando usuario não esta ativado e deve ativar o usuario`() {
-//        val token = "validToken"
-//        val usuario = Usuario(
-//            "Nome",
-//            "Sobre",
-//            "teste@teste.com",
-//            BCryptPasswordEncoder().encode("password"),
-//            Papel.PADRAO,
-//            false,
-//            TipoCadastro.PADRAO,
-//            token
-//        )
-//
-//        mockMvc.perform(
-//            patch("/v1/usuario/ativar")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(AtivarUsuarioRequest(token)))
-//        )
-//            .andExpect(status().isNoContent)
-//    }
-//
-//    @Test
-//    fun `should return 200 OK when user is already active`() {
-//
-//        val token = "validToken"
-//
-//        val request = criarAtivarUsuarioRequest(token)
-//        val requestJson = objectMapper.writeValueAsString(request)
-//
-//        mockMvc.perform(
-//            patch("/v1/usuario/ativar")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(requestJson)
-//        )
-//            .andDo(print())
-//            .andExpect(status().isOk)
-//    }
+    @Test
+    fun `deve retornar 204 No Content quando usuario não esta ativado e deve ativar o usuario`() {
+
+        mockMvc.perform(
+            patch("/v1/usuario/ativar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(AtivarUsuarioRequest(tokenTeste)))
+        )
+            .andDo(print())
+            .andExpect(status().isNoContent)
+    }
+
+    @Test
+    fun `should return 200 OK when user is already active`() {
+
+        val request = criarAtivarUsuarioRequest(tokenTeste)
+        val requestJson = objectMapper.writeValueAsString(request)
+
+        mockMvc.perform(
+            patch("/v1/usuario/ativar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+    }
 
     @Test
     fun `deve retornar 400 bad request quando usario não encontrado por token`() {
