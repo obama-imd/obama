fb07f7d37759ca9cef9273950ee2bf10cdf112c9
Emanuel Kywal <emanuelkywal@outlook.com>
2024-06-26 10:04:03 -0400

    Refatora DTO-Exchange ListarAnoEnsinoResponse

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/exchange/ListarAnoEnsinoResponse.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/exchange/ListarAnoEnsinoResponse.kt
@@ -1,7 +1,5 @@
 package br.ufrn.imd.obama.oa.infrastructure.resource.exchange
 
-import br.ufrn.imd.obama.oa.domain.model.NivelEnsino
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
@@ -9,9 +7,6 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming
 data class ListarAnoEnsinoResponse(
     var id: Long,
 
-    var nome: String,
-
-    @JsonProperty("nivel_ensino")
-    var nivelEnsino: NivelEnsino
+    var nome: String
 )
 
