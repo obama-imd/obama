1f4f999767ac8ee6d761d11b6a930836c0ab4c9c
RamonJales <ramonjales123@gmail.com>
2024-08-03 21:32:17 -0300

    response buscar por id

---
+++ src/main/kotlin/br/ufrn/imd/obama/planoaula/infrastructure/resource/exchange/PlanoAulaBuscarPorIdResponse.kt
@@ -0,0 +1,22 @@
+package br.ufrn.imd.obama.planoaula.infrastructure.resource.exchange
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy::class)
+class PlanoAulaBuscarPorIdResponse(
+
+    val id: Long,
+    val titulo: String?,
+    val escola: String?,
+    val resumo: String?,
+    val objetivoGeral: String?,
+    val objetivosEspecificos: String?,
+    val metodologia: String?,
+    val referencias: String?,
+    val nivelEnsinoId: Long?,
+    val idDisciplinas: List<Long>,
+    val anoEnsinoId: Long?,
+    val emailCoatores: List<String>,
+    val idObjetosAprendizagem: List<Long>
+)
