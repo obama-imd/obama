6c536e81aa9b873cc17e838fb74c008d2ccf0851
leirdan <uouleirdan@gmail.com>
2024-10-21 13:29:09 -0300

    OB-0026: Recurso de buscar planos de aula por coautor
    (Ainda não foi avaliada a necessidade de usar cache)

--- src/main/kotlin/br/ufrn/imd/obama/planoaula/infrastructure/resource/PlanoAulaResource.kt
+++ src/main/kotlin/br/ufrn/imd/obama/planoaula/infrastructure/resource/PlanoAulaResource.kt
@@ -15,22 +15,24 @@ import org.springframework.security.core.userdetails.UserDetails
 
 interface PlanoAulaResource {
     @Operation(summary = "Endpoint para buscar planos de aula por título")
-    @ApiResponses(value = [
-        ApiResponse(
-            responseCode = "200",
-            description = "Planos de aula encontrados",
-            content = [
-                Content(
-                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = Schema(implementation = Page::class)
-                )
-            ]
-        ),
-        ApiResponse(
-            responseCode = "403",
-            description = "Usuário não autenticado",
-        )
-    ])
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Planos de aula encontrados",
+                content = [
+                    Content(
+                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = Schema(implementation = Page::class)
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Usuário não autenticado",
+            )
+        ]
+    )
     fun buscarPlanosAulaPorTitulo(
         usuario: UserDetails,
         titulo: String?,
@@ -38,26 +40,54 @@ interface PlanoAulaResource {
     ): Page<PlanoAulaResponse>
 
     @Operation(summary = "Endpoint para buscar planos de aula por id")
-    @ApiResponses(value = [
-        ApiResponse(
-            responseCode = "200",
-            description = "Plano de aula encontrado",
-            content = [
-                Content(
-                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = Schema(implementation = PlanoAulaResponse::class)
-                )
-            ]
-        ),
-        ApiResponse(
-            responseCode = "403",
-            description = "Usuário não autenticado; o plano de aula não pertece ao usuário ou usuário não é coator",
-        ),
-        ApiResponse(
-            responseCode = "404",
-            description = "Plano de aula não encontrado",
-        )
-    ])
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Plano de aula encontrado",
+                content = [
+                    Content(
+                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = Schema(implementation = PlanoAulaResponse::class)
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Usuário não autenticado; o plano de aula não pertece ao usuário ou usuário não é coator",
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Plano de aula não encontrado",
+            )
+        ]
+    )
     fun buscarPlanoAulaPorId(id: Long): PlanoAulaBuscarPorIdResponse
+
+    @Operation(summary = "Endpoint para buscar planos de aula por coautor")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Planos de aula encontrados",
+                content = [
+                    Content(
+                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = Schema(implementation = PlanoAulaResponse::class)
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Usuário não autenticado; o plano de aula não pertece ao usuário ou usuário não é coautor",
+            )
+        ]
+    )
+
+    fun buscarPlanosAulaPorCoautor(
+        usuario: UserDetails,
+        titulo: String?,
+        pageable: Pageable
+    ): Page<PlanoAulaResponse>
 
 }
--- src/main/kotlin/br/ufrn/imd/obama/planoaula/infrastructure/resource/PlanoAulaResourceImpl.kt
+++ src/main/kotlin/br/ufrn/imd/obama/planoaula/infrastructure/resource/PlanoAulaResourceImpl.kt
@@ -14,11 +14,7 @@ import org.springframework.http.MediaType
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("v1/planoaula")
@@ -29,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController
 )
 class PlanoAulaResourceImpl(
     private val planoAulaUseCase: PlanoAulaUseCase
-): PlanoAulaResource {
+) : PlanoAulaResource {
     @Cacheable(cacheNames = ["planosaula"])
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     override fun buscarPlanosAulaPorTitulo(
@@ -40,11 +36,21 @@ class PlanoAulaResourceImpl(
 
         val autor = usuario as UsuarioEntity
 
-        return planoAulaUseCase.buscarPlanoAulaPorTitulo(autor, titulo, pageable).map{ it.toResponse() }
+        return planoAulaUseCase.buscarPlanoAulaPorTitulo(autor, titulo, pageable).map { it.toResponse() }
     }
 
     @GetMapping(path = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     override fun buscarPlanoAulaPorId(@PathVariable("id", required = true) id: Long): PlanoAulaBuscarPorIdResponse {
         return planoAulaUseCase.buscarPlanoAulaPorId(id).toPlanoAulaBuscarPorIdResponse()
+    }
+
+    @GetMapping(path = ["/buscarPorCoautor"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    override fun buscarPlanosAulaPorCoautor(
+        @AuthenticationPrincipal usuario: UserDetails,
+        @RequestParam(value = "titulo", required = false) titulo: String?,
+        pageable: Pageable
+    ): Page<PlanoAulaResponse> {
+        val coautor = usuario as UsuarioEntity
+        return planoAulaUseCase.buscarPlanosAulaPorCoautor(coautor, titulo, pageable).map { it.toResponse() }
     }
 }
