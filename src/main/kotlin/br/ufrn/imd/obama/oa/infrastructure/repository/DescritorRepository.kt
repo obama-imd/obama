b417fa39f1ca53a6ebae4bf7b8bc3f5ef973e4fe
Samuel.clipes <121581663+samuel123d@users.noreply.github.com>
2025-05-16 12:18:42 -0300

    feat: DescritorRepository method to list with params NivelEnsino and TemaConteudo

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/DescritorRepository.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/DescritorRepository.kt
@@ -1,6 +1,8 @@
 package br.ufrn.imd.obama.oa.infrastructure.repository
 
 import br.ufrn.imd.obama.oa.infrastructure.entity.DescritorEntity
+import br.ufrn.imd.obama.oa.infrastructure.entity.NivelEnsinoEntity
+import br.ufrn.imd.obama.oa.infrastructure.entity.TemaConteudoEntity
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -9,4 +11,10 @@ import org.springframework.stereotype.Repository
 @Repository
 interface DescritorRepository: JpaRepository<DescritorEntity, Long> {
     fun findAllByOrderByCodigoAsc(page: Pageable): Page<DescritorEntity>
+
+    fun findAllByNivelEnsinoAndTemaConteudo(
+        nivelEnsino: NivelEnsinoEntity,
+        temaConteudo: TemaConteudoEntity,
+        pageable: Pageable
+    ): Page<DescritorEntity>
 }
