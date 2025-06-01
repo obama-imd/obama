7e3f4234bd4ac940507afa60cdd29a419e7e551c
RamonJales <ramonjales123@gmail.com>
2025-05-31 22:04:00 -0300

    adicionado todo

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/AnoEnsinoRepository.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/repository/AnoEnsinoRepository.kt
@@ -7,6 +7,8 @@ import java.util.Optional
 
 @Repository
 interface AnoEnsinoRepository : JpaRepository<AnoEnsinoEntity, Long> {
-
-    fun findById(id: Long?): Optional<AnoEnsinoEntity?>
+    //TODO: o parâmetro deveria ser Long, ou seja, ele não deveria ser nullable. Não faz sentido no contexto de um banco de dados relacional,
+    // pois o ID de uma entidade (chave primária) nunca deve ser null. Passar null para findById geralmente resulta em um comportamento indefinido
+    // ou uma consulta que não retorna resultados. Modifique isso, em vários locais está sendo usado Long? como parâmetro.
+    fun findById(id: Long?): Optional<AnoEnsinoEntity>
 }
