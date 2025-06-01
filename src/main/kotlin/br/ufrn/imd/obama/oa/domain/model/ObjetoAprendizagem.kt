9409a8461fc38d9f09098c0ea0f89822428b6352
RamonJales <ramonjales123@gmail.com>
2025-05-31 22:06:54 -0300

    design pattern builder implementado

--- src/main/kotlin/br/ufrn/imd/obama/oa/domain/model/ObjetoAprendizagem.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/domain/model/ObjetoAprendizagem.kt
@@ -3,7 +3,7 @@ package br.ufrn.imd.obama.oa.domain.model
 import java.time.LocalDate
 
 
-class ObjetoAprendizagem(
+class ObjetoAprendizagem private constructor(
 
 	val id: Long,
 
@@ -33,7 +33,61 @@ class ObjetoAprendizagem(
 
 	val plataformas: List<ObjetoAprendizagemPlataforma>,
 ) {
+	companion object {
+		fun create(
+			id: Long,
+			nome: String,
+			descricao: String,
+			quantidadeAcessos: Int,
+			thumbnailPath: String?,
+			dataLancamento: LocalDate?,
+			versao: String?,
+			ativo: Boolean,
+			tipoLicensaUso: TipoLicensaUso?,
+			idiomas: Set<Idioma>?,
+			autoresMantenedores: Set<AutorMantenedor>,
+			descritores: Set<Descritor>,
+			habilidades: Set<Habilidade>,
+			plataformas: List<ObjetoAprendizagemPlataforma>
+		): ObjetoAprendizagem {
+			return ObjetoAprendizagem(
+				id,
+				nome,
+				descricao,
+				quantidadeAcessos,
+				thumbnailPath,
+				dataLancamento,
+				versao,
+				ativo,
+				tipoLicensaUso,
+				idiomas,
+				autoresMantenedores,
+				descritores,
+				habilidades,
+				plataformas
+			)
+		}
+	}
+
 	override fun equals(other: Any?): Boolean {
 		return id == (other as ObjetoAprendizagem).id
 	}
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + quantidadeAcessos
+        result = 31 * result + ativo.hashCode()
+        result = 31 * result + nome.hashCode()
+        result = 31 * result + descricao.hashCode()
+        result = 31 * result + (thumbnailPath?.hashCode() ?: 0)
+        result = 31 * result + (dataLancamento?.hashCode() ?: 0)
+        result = 31 * result + (versao?.hashCode() ?: 0)
+        result = 31 * result + (tipoLicensaUso?.hashCode() ?: 0)
+        result = 31 * result + (idiomas?.hashCode() ?: 0)
+        result = 31 * result + autoresMantenedores.hashCode()
+        result = 31 * result + descritores.hashCode()
+        result = 31 * result + habilidades.hashCode()
+        result = 31 * result + plataformas.hashCode()
+        return result
+    }
 }
