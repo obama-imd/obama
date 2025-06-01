dd1f66d12d5972f5403797cddbbd012855c7b091
RamonJales <ramonjales123@gmail.com>
2025-05-31 22:05:51 -0300

    setup oa levando em consideração outras entidades

--- src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/configuration/OaConfig.kt
+++ src/main/kotlin/br/ufrn/imd/obama/oa/infrastructure/configuration/OaConfig.kt
@@ -1,5 +1,11 @@
 package br.ufrn.imd.obama.oa.infrastructure.configuration
 
+import br.ufrn.imd.obama.oa.domain.gateway.AutorMantenedorGateway
+import br.ufrn.imd.obama.oa.domain.gateway.DescritorDatabaseGateway
+import br.ufrn.imd.obama.oa.domain.gateway.HabilidadeGateway
+import br.ufrn.imd.obama.oa.domain.gateway.IdiomaGateway
+import br.ufrn.imd.obama.oa.domain.gateway.ObjetoAprendizagemPlataformaGateway
+import br.ufrn.imd.obama.oa.domain.gateway.TipoLicensaUsoGateway
 import br.ufrn.imd.obama.oa.domain.usecase.ObjetoAprendizagemUseCase
 import br.ufrn.imd.obama.oa.domain.usecase.ObjetoAprendizagemUseCaseImpl
 import br.ufrn.imd.obama.oa.infrastructure.adapter.ObjetoAprendizagemDatabaseGatewayAdapter
@@ -12,10 +18,23 @@ class OaConfig {
 
     @Bean
     @Primary
-    fun setUpBuscarOa(oaGatewayAdapter: ObjetoAprendizagemDatabaseGatewayAdapter
+    fun setUpBuscarOa(
+        oaGatewayAdapter: ObjetoAprendizagemDatabaseGatewayAdapter,
+        autorMantenedorGateway: AutorMantenedorGateway,
+        descritorGateway: DescritorDatabaseGateway,
+        habilidadeGateway: HabilidadeGateway,
+        plataformaGateway: ObjetoAprendizagemPlataformaGateway,
+        tipoLicensaUsoGateway: TipoLicensaUsoGateway,
+        idiomaGateway: IdiomaGateway
     ): ObjetoAprendizagemUseCase {
         return ObjetoAprendizagemUseCaseImpl(
-                oaGatewayAdapter
+            oaGateway = oaGatewayAdapter,
+            autorMantenedorGateway = autorMantenedorGateway,
+            descritorGateway = descritorGateway,
+            habilidadeGateway = habilidadeGateway,
+            plataformaGateway = plataformaGateway,
+            tipoLicensaUsoGateway = tipoLicensaUsoGateway,
+            idiomaGateway = idiomaGateway
         )
     }
 }
