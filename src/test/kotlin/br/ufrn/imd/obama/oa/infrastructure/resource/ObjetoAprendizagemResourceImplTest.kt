5ea3a8984da0e589424559ecb364fb3941b23dc7
Vitor Henrique <vitorhenrique908@gmail.com>
2025-11-15 22:15:34 -0300

    OB-0015: Resolve problemas de segurança, atualiza o java, atualiza as dependências e o gitflow

--- src/test/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/ObjetoAprendizagemResourceImplTest.kt
+++ src/test/kotlin/br/ufrn/imd/obama/oa/infrastructure/resource/ObjetoAprendizagemResourceImplTest.kt
@@ -1,11 +1,14 @@
 package br.ufrn.imd.obama.oa.infrastructure.resource
 
 import br.ufrn.imd.obama.oa.domain.usecase.ObjetoAprendizagemUseCase
+import br.ufrn.imd.obama.oa.infrastructure.entity.ObjetoAprendizagemEntity
 import br.ufrn.imd.obama.oa.infrastructure.mapper.toEntity
 import br.ufrn.imd.obama.oa.infrastructure.repository.ObjetoAprendizagemRepository
 import br.ufrn.imd.obama.oa.util.criarObjetoAprendizagem
 import br.ufrn.imd.obama.usuario.infrastructure.configuration.SecurityConfiguration
 import br.ufrn.imd.obama.usuario.infrastructure.configuration.TokenConfiguration
+import jakarta.persistence.EntityManager
+import org.springframework.transaction.annotation.Transactional
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -19,39 +22,68 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
-@Import(value=
-    [
-        TokenConfiguration::class,
-        SecurityConfiguration::class
-    ]
-)
+@Import(value = [TokenConfiguration::class, SecurityConfiguration::class])
 @AutoConfigureMockMvc
 @ActiveProfiles(profiles = ["test"])
 class ObjetoAprendizagemResourceImplTest {
 
+    @Autowired private lateinit var mockMvc: MockMvc
 
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+    @Autowired private lateinit var objetoAprendizagemUseCase: ObjetoAprendizagemUseCase
 
-    @Autowired
-    private  lateinit var objetoAprendizagemUseCase: ObjetoAprendizagemUseCase
-
-    @Autowired
-    private lateinit var objetoAprendizagemRepository: ObjetoAprendizagemRepository
+    @Autowired private lateinit var entityManager: EntityManager
 
     @Test
+    @Transactional
     fun `Deve retornar OK quando informado id existente`() {
-        val resultado = criarObjetoAprendizagem().toEntity()
-
-        objetoAprendizagemRepository.save(resultado)
+        val resultado = adicionarObjetoAprendizagemExistente()
 
         mockMvc.perform(
-            MockMvcRequestBuilders.get("/v1/oa/{id}", resultado.id)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                        MockMvcRequestBuilders.get("/v1/oa/{id}", resultado.id)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+    }
 
+    private fun adicionarObjetoAprendizagemExistente(): ObjetoAprendizagemEntity {
+        val resultado: ObjetoAprendizagemEntity = criarObjetoAprendizagem().toEntity()
+
+        val sql = """
+            INSERT INTO objeto_aprendizagem(
+                id,
+                ativo,
+                data_lancamento,
+                descricao,
+                nome,
+                caminho_thumbnail,
+                quantidade_acessos,
+                versao
+            ) VALUES (
+                :id,
+                :ativo,
+                :dataLancamento,
+                :descricao,
+                :nome,
+                :caminhoThumbnail,
+                :quantidadeAcessos,
+                :versao
+            );
+        """.trimIndent()
+
+        entityManager.createNativeQuery(sql)
+            .setParameter("id", resultado.id)
+            .setParameter("ativo", resultado.ativo)
+            .setParameter("dataLancamento", resultado.dataLancamento)
+            .setParameter("descricao", resultado.descricao)
+            .setParameter("nome", resultado.nome)
+            .setParameter("caminhoThumbnail", resultado.thumbnailPath)
+            .setParameter("quantidadeAcessos", resultado.quantidadeAcessos)
+            .setParameter("versao", resultado.versao)
+            .executeUpdate()
+
+        return resultado
     }
 
     @Test
@@ -59,23 +91,24 @@ class ObjetoAprendizagemResourceImplTest {
         val idInexistente = 0L
 
         mockMvc.perform(
-            MockMvcRequestBuilders.get("/v1/oa/{id}", idInexistente)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().isNotFound)
+                        MockMvcRequestBuilders.get("/v1/oa/{id}", idInexistente)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound)
     }
 
     @Test
     fun `Deve retornar retornar lista de objetos`() {
 
-        mockMvc.perform (
-            MockMvcRequestBuilders.get("/v1/oa")
-                .param("page", "0")
-                .param("size", "10")
-                .param("nome", "Math")
-        ).andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/v1/oa")
+                                .param("page", "0")
+                                .param("size", "10")
+                                .param("nome", "Math")
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
     }
 }
